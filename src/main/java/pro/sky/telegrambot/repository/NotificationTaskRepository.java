9435dd9f0c3f29249b8c38ae828cbe4df278d306
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-04-03 19:15:11 +0300

    add query in repository

--- src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.sky.telegrambot.model.NotificationTask;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+
 @Repository
 public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
+    Collection<NotificationTask> findByDateTime(LocalDateTime dateTime);
 }
